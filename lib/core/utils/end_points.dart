143d84cd0adfaf3086ffbc8b1bcce3b2f314b9ff
MohamedNasrr <mohamednasrrkh@gmail.com>
2025-06-06 00:21:01 +0300

    add category endpoint

--- lib/core/utils/end_points.dart
+++ lib/core/utils/end_points.dart
@@ -1,4 +1,5 @@
 abstract class ApiEndPoints{
   static const String products= '/products';
   static const String search = '/products/search';
+  static const String category = '/products/categories';
 }
