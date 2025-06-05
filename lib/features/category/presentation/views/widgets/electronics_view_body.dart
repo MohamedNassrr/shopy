0074f9f1d95c24fdd36eb2bbc4a02d244d37a019
MohamedNasrr <mohamednasrrkh@gmail.com>
2025-06-06 00:20:27 +0300

    add decor/electronics screens

---
+++ lib/features/category/presentation/views/widgets/electronics_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ElectronicsViewBody extends StatelessWidget {
+  const ElectronicsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('Electronics'),
+      ],
+    );
+  }
+}
